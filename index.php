095dea951b58e4e58889e3aa61c26946c347bc47
mikey <michaelrayvon@gmail.com>
2015-05-05 16:56:20 -0500

    feed markup

--- index.php
+++ index.php
@@ -13,13 +13,74 @@ get_header();
 if( have_posts() ) {
   while( have_posts() ) {
     the_post();
+    $post_type = get_post_type();
 ?>
 
     <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-      <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-
-      <?php the_content(); ?>
+<?php 
+if ( $post_type == 'post' ) { 
+?>
+      <div class="container container-small">
+        <div class="row">
+          <div class="col into-2">
+            <a href="<?php the_permalink() ?>"><?php the_post_thumbnail( 'post-thumb' ); ?></a>
+          </div>
+          <div class="col into-2">
+            <span class="date"><?php the_date( ); ?></span>
+            <h2>
+              <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+            </h2>
+            <?php the_content(); ?>
+          </div>
+        </div>
+      </div>
+<?php 
+} else if ( $post_type == 'lookbook') { 
+?>
+      <div class="container container-medium">
+        <div class="row">
+          <div class="col into-1">
+            <?php the_post_thumbnail( 'post-thumb' ); ?>
+          </div>
+        </div>
+      </div>
+      <div class="container container-small">
+        <div class="row">
+          <div class="col into-2">
+            <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+          </div>
+          <div class="col into-2">
+            <?php the_content(); ?>
+          </div>
+        </div>
+      </div>
+<?php 
+} else if ( $post_type == 'product') { 
+?>
+      <div class="container container-medium">
+        <div class="row">
+          <div class="col into-2">
+            <!-- PRODUCT IMAGE 1 -->
+          </div>
+          <div class="col into-2">
+            <!-- PRODUCT IMAGE 2 -->
+          </div>
+        </div>
+      </div>
+      <div class="container container-small">
+        <div class="row">
+          <div class="col into-2">
+            <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+          </div>
+          <div class="col into-2">
+            <?php the_content(); ?>
+          </div>
+        </div>
+      </div>
+<?php 
+} 
+?>
 
     </article>
 
@@ -27,10 +88,15 @@ if( have_posts() ) {
   }
 } else {
 ?>
-    <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+    <article class="u-alert">
+      <div class="container container-small">
+        <?php _e('Sorry, no posts matched your criteria :{'); ?>
+      </div>
+    </article>
 <?php
 } ?>
-
+    
+    </div>
   <!-- end posts -->
   </section>
 
