8e67e8abd022e47bc89ac3a85269594989f84cc4
mikey <michaelrayvon@gmail.com>
2015-05-06 15:44:26 -0500

    init

---
+++ archive-entry.php
@@ -0,0 +1,26 @@
+<?php
+$post_type = get_post_type();
+$excerpt = get_the_excerpt();
+?>
+<article class="col into-3">
+  <a href="<?php the_permalink() ?>"><?php the_post_thumbnail( 'feed-small' ); ?></a>
+  <h2>
+    <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+  </h2>
+  <span class="date"><?php the_date( ); ?></span>
+<?php 
+if ( $post_type == 'post' ) { 
+?>
+  &nbsp;&nbsp;&mdash;&nbsp;&nbsp;<a href="<?php echo get_post_type_archive_link( 'post' ); ?>"><span class="fa fa-thumb-tack"></span></a></p>
+<?php 
+} else if ( $post_type == 'lookbook') { 
+?>
+  &nbsp;&nbsp;&mdash;&nbsp;&nbsp;<a href="<?php echo get_post_type_archive_link( 'lookbook' ); ?>"><span class="fa fa-eye"></span></a></p>
+<?php 
+} else if ( $post_type == 'product') { 
+?>
+  &nbsp;&nbsp;&mdash;&nbsp;&nbsp;<a href="<?php echo get_post_type_archive_link( 'product' ); ?>"><span class="fa fa-shopping-cart"></span></a></p>
+<?php 
+} 
+?>
+</article>
