16f172c14a9258dac5c00092cc48fac85878cd08
mikey <michaelrayvon@gmail.com>
2015-05-06 18:19:30 -0500

    add arrow svgs

--- partials/pagination.php
+++ partials/pagination.php
@@ -6,8 +6,11 @@ if( get_next_posts_link() || get_previous_posts_link() ) {
   	<div class="container container-small">
 	  	<div class="row">
 <?php
-$previous = get_previous_posts_link('Newer');
-$next = get_next_posts_link('Older');
+$right = url_get_contents( get_bloginfo('stylesheet_directory') . '/img/arrow-right.svg' );
+$left = url_get_contents( get_bloginfo('stylesheet_directory') . '/img/arrow-left.svg' );
+
+$previous = get_previous_posts_link($left);
+$next = get_next_posts_link($right);
 if ($previous && $next) {
 ?>
 				<span class="col into-2 u-align-left"><?php echo $previous; ?></span>
