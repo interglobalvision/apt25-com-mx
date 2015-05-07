e0f190c4e5521bb6181c3b0aa7a5284be95243ae
mikey <michaelrayvon@gmail.com>
2015-05-07 13:19:53 -0500

    move dash

--- archive-entry.php
+++ archive-entry.php
@@ -3,23 +3,23 @@ $post_type = get_post_type( $entry_id );
 // >> if you use setup_postdata() in here you can use normal the_title() type tags
 
 ?>
-  <a href="<?php echo get_the_permalink( $entry_id ) ?>"><?php echo get_the_post_thumbnail( $entry_id, 'feed-small' ); ?></a>
+  <a href="<?php echo get_the_permalink( $entry_id ) ?>"><?php echo get_the_post_thumbnail( $entry_id, 'feed-small', 'class=archive-entry-img' ); ?></a>
   <h2>
     <a href="<?php echo get_the_permalink( $entry_id ) ?>"><?php echo get_the_title( $entry_id ); ?></a>
   </h2>
-  <span class="date"><?php echo get_the_date( null, $entry_id ); ?></span>
+  <span class="date"><?php echo get_the_date( null, $entry_id ); ?></span>&nbsp;&nbsp;&mdash;&nbsp;&nbsp;
 <?php
 if ( $post_type == 'post' ) {
 ?>
-  &nbsp;&nbsp;&mdash;&nbsp;&nbsp;<a href="<?php echo get_bloginfo( 'url' ) . '/posts/'; ?>"><span class="fa fa-thumb-tack"></span></a></p>
+  <a href="<?php echo get_bloginfo( 'url' ) . '/posts/'; ?>"><span class="fa fa-thumb-tack"></span></a></p>
 <?php
 } else if ( $post_type == 'lookbook') {
 ?>
-  &nbsp;&nbsp;&mdash;&nbsp;&nbsp;<a href="<?php echo get_post_type_archive_link( 'lookbook' ); ?>"><span class="fa fa-eye"></span></a></p>
+  <a href="<?php echo get_post_type_archive_link( 'lookbook' ); ?>"><span class="fa fa-eye"></span></a></p>
 <?php
 } else if ( $post_type == 'product') {
 ?>
-  &nbsp;&nbsp;&mdash;&nbsp;&nbsp;<a href="<?php echo get_post_type_archive_link( 'product' ); ?>"><span class="fa fa-shopping-cart"></span></a></p>
+  <a href="<?php echo get_post_type_archive_link( 'product' ); ?>"><span class="fa fa-shopping-cart"></span></a></p>
 <?php
 }
 ?>
