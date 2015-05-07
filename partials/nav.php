af2b5abf7d9c739eb40d2173e1da54f5d9f00393
mikey <michaelrayvon@gmail.com>
2015-05-07 15:49:11 -0500

    init

---
+++ partials/nav.php
@@ -0,0 +1,24 @@
+<nav class="main-menu u-bold-cn">
+  <ul>
+<?php
+$shop_url = IGV_get_option('_igv_shop_url');
+if (! empty($shop_url)) {
+?>
+    <li class="menu-item js-svg-container">
+      <a href="<?php echo $shop_url; ?>">Shop</a>
+      <?php echo url_get_contents( get_bloginfo('stylesheet_directory') . '/img/smile.svg' ); ?>
+    </li>
+<?php 
+}
+?>
+    <li class="menu-item js-svg-container">
+      <a href="<?php echo get_bloginfo('url') . '/archive/'; ?>">Archive</a>
+      <?php echo url_get_contents( get_bloginfo('stylesheet_directory') . '/img/squiggle.svg' ); ?>
+    </li>
+    <li class="menu-item">
+      <form action="/" method="get" class="u-bold-cn">
+        <input type="text" name="s" class="u-bold-cn" id="search" placeholder="SEARCH" svalue="<?php the_search_query(); ?>" />
+      </form>
+    </li>
+  </ul>
+</nav>
