ee78b6ef00bc4c2c9b6b8cb172262c953087ee2c
mikey <michaelrayvon@gmail.com>
2015-05-06 19:24:33 -0500

    move related posts into related.php

--- single.php
+++ single.php
@@ -69,57 +69,7 @@ if( have_posts() ) {
       </div>
     </article>
 
-    <div class="container container-small">
-      <div class="row">
-<?php 
-    
-    $related_id = get_post_meta( $post->ID, '_igv_related1', true );
-
-    if ( empty( $related_id_1 ) ) {
-      $rand_args = array(
-        'orderby' => 'rand',
-        'post_type' => array('post','lookbook','product'),
-        'post__not_in' => array($post->ID),
-        'numberposts' => 1,
-      );
-      $rand_posts = get_posts($rand_args);
-      foreach ($rand_posts as $rand_post) {
-        $entry_id = $rand_post->ID;
-      }
-    } else {
-      $entry_id = $related_id;
-    }
-
-?>
-        <article class="col into-2">
-          <?php include(locate_template('archive-entry.php')); ?>      
-        </article>
-<?php
-    $related_id = null;
-    $related_id = get_post_meta( $post->ID, '_igv_related2', true );
-
-    if ( empty( $related_id ) || $related_id == $entry_id ) {
-      $rand_args = array(
-        'orderby' => 'rand',
-        'post_type' => array('post','lookbook','product'),
-        'post__not_in' => array($post->ID, $entry_id),
-        'numberposts' => 1,
-      );
-      $rand_posts = get_posts($rand_args);
-      foreach ($rand_posts as $rand_post) {
-        $entry_id = $rand_post->ID;
-      }
-    } else {
-      $entry_id = $related_id;
-    }
-
-?>
-        <article class="col into-2">
-          <?php include(locate_template('archive-entry.php')); ?>      
-        </article>
-
-      </div>
-    </div>
+    <?php get_template_part( 'related' ); ?>
 
 <?php
   } // end while
