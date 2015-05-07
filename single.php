423ed62f35a2ca966c9930cc5ab4b6203a6583fd
mikey <michaelrayvon@gmail.com>
2015-05-06 19:06:26 -0500

    fix random related posts

--- single.php
+++ single.php
@@ -72,31 +72,51 @@ if( have_posts() ) {
     <div class="container container-small">
       <div class="row">
 <?php 
-    for ($i = 1; $i <= 2; $i++) {
-      $entry_id = null;
-      $entry_id = get_post_meta( $post->ID, '_igv_related' . $i, true );
-      if ( empty( $entry_id ) ) {
-        $rand_args = array(
-          'orderby' => 'rand',
-          'post_type' => array('post','lookbook','product'),
-          'post__not_in' => array($post->ID),
-          'numberposts'=>1,
-        );
-        $rand_posts = get_posts($rand_args);
-        foreach ($rand_posts as $rand_post) {
-          $entry_id = $rand_post->ID;
-        }
-      }
-?>
+    
+    $related_id = get_post_meta( $post->ID, '_igv_related1', true );
 
+    if ( empty( $related_id_1 ) ) {
+      $rand_args = array(
+        'orderby' => 'rand',
+        'post_type' => array('post','lookbook','product'),
+        'post__not_in' => array($post->ID),
+        'numberposts' => 1,
+      );
+      $rand_posts = get_posts($rand_args);
+      foreach ($rand_posts as $rand_post) {
+        $entry_id = $rand_post->ID;
+      }
+    } else {
+      $entry_id = $related_id;
+    }
+
+?>
         <article class="col into-2">
           <?php include(locate_template('archive-entry.php')); ?>      
         </article>
-       
 <?php
-      wp_reset_postdata();
-    } // end for
+    $related_id = null;
+    $related_id = get_post_meta( $post->ID, '_igv_related2', true );
+
+    if ( empty( $related_id ) || $related_id == $entry_id ) {
+      $rand_args = array(
+        'orderby' => 'rand',
+        'post_type' => array('post','lookbook','product'),
+        'post__not_in' => array($post->ID, $entry_id),
+        'numberposts' => 1,
+      );
+      $rand_posts = get_posts($rand_args);
+      foreach ($rand_posts as $rand_post) {
+        $entry_id = $rand_post->ID;
+      }
+    } else {
+      $entry_id = $related_id;
+    }
+
 ?>
+        <article class="col into-2">
+          <?php include(locate_template('archive-entry.php')); ?>      
+        </article>
 
       </div>
     </div>
