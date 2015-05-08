094aafcc15e24e5923ffa7c64896c0de8878ab49
mikey <michaelrayvon@gmail.com>
2015-05-08 10:43:32 -0500

    init

---
+++ partials/subscribe.php
@@ -0,0 +1,4 @@
+<form action="//apt25.us10.list-manage.com/subscribe/post?u=226b6a2b7121635950909f5d3&amp;id=d7b454101b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+	<input type="email" value="" name="EMAIL" class="email expand" id="mce-EMAIL" placeholder="email">
+  <input type="submit" value="SUBSCRIBE" name="subscribe" id="subscribe" class="button u-bold-cn">
+</form>
