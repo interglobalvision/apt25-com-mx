35f8cf84d670b2f73b7b6badc3fe88dda3a39150
mikey <michaelrayvon@gmail.com>
2015-05-08 11:53:45 -0500

    add expand class, zigzag

--- partials/subscribe.php
+++ partials/subscribe.php
@@ -1,4 +1,7 @@
 <form action="//apt25.us10.list-manage.com/subscribe/post?u=226b6a2b7121635950909f5d3&amp;id=d7b454101b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-	<input type="email" value="" name="EMAIL" class="email expand" id="mce-EMAIL" placeholder="email">
+	<div class="expand">
+		<input type="email" value="" name="EMAIL" class="email expand" id="mce-EMAIL" placeholder="email">
+		<?php echo url_get_contents( get_bloginfo('stylesheet_directory') . '/img/zigzag.svg' ); ?>
+	</div>
   <input type="submit" value="SUBSCRIBE" name="subscribe" id="subscribe" class="button u-bold-cn">
 </form>
