41cb30f9bedc2fa4d723c671b3126852f7f6c06b
cas <casska@gmail.com>
2015-08-11 15:10:44 -0500

    fix back to 1x images for testing(?)

--- lib/gallery.php
+++ lib/gallery.php
@@ -33,7 +33,7 @@ function my_gallery_shortcode($attr) {
 		'icontag'    => 'li',
 		'captiontag' => 'span',
 		'columns'    => 3,
-		'size'       => 'image-large',
+		'size'       => 'image-basic',
 		'include'    => '',
 		'exclude'    => ''
 	), $attr));
